82f2f093b253dfdadcb893d0b3e69e3ba4ddd213
Raoul1808 <raoulthegeek@gmail.com>
2023-07-31 09:36:37 +0200

    Basic Discord bot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 from dotenv import load_dotenv
 
+import discord
 
-def show_token():
-    token = os.getenv('DISCORD_BOT_TOKEN')
-    print('Found token in environment variables:', token)
+
+class StickountClient(discord.Client):
+    async def on_connect(self):
+        print("Connected to Discord")
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} ; {self.user.id}")
 
 
 if __name__ == '__main__':
     load_dotenv()
-    show_token()
+
+    intents = discord.Intents.default()
+    intents.emojis_and_stickers = True
+
+    client = StickountClient(intents=intents)
+    client.run(os.getenv('DISCORD_BOT_TOKEN'))
