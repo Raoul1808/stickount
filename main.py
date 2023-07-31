7fb66e610035628da2c4a79741614e0a1d66fd96
Raoul1808 <raoulthegeek@gmail.com>
2023-07-31 09:20:54 +0200

    Add dotenv dependency

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+def show_token():
+    token = os.getenv('DISCORD_BOT_TOKEN')
+    print('Found token in environment variables:', token)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    load_dotenv()
+    show_token()
