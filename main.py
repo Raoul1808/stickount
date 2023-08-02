905ed0b11011ccc94199facb47182cbce32e22b7
Raoul1808 <raoulthegeek@gmail.com>
2023-08-02 13:23:03 +0200

    Set count command

--- main.py
+++ main.py
@@ -37,6 +37,22 @@ async def stickount(interaction):
     await interaction.response.send_message(msg)
 
 
+@tree.command(name="setcount", description="Set sticker count on a sticker", guild=discord.Object(id=SOURCE_GUILD))
+@app_commands.checks.has_permissions(manage_expressions=True)
+async def setcount(interaction, count: int, sticker_name: str):
+    if count < 0:
+        await interaction.response.send_message("Cannot set negative sticker count", ephemeral=True)
+        return
+    stickers = await client.get_guild(SOURCE_GUILD).fetch_stickers()
+    for sticker in stickers:
+        if sticker.name == sticker_name:
+            STICKER_USE_COUNT[sticker.id].count = count
+            await interaction.response.send_message(f"Successfully set sticker `{sticker_name}` count to {count}", ephemeral=True)
+            break
+    else:
+        await interaction.response.send_message(f"Failed to find sticker matching name `{sticker_name}`", ephemeral=True)
+
+
 @client.event
 async def on_connect():
     print("Connected to Discord")
@@ -50,6 +66,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author == client.user or message.author.bot:
+        return
+
     if message.guild.id != SOURCE_GUILD:
         return
 
